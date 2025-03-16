17ba0e43d462e54d3032e4f407dcdbd340dce9a5
float321 <187214682+float321@users.noreply.github.com>
2025-03-16 11:44:13 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,1 +1,23 @@
+import requests
+import sqlite3
 
+
+
+lt = []
+t = 0
+while len(lt) != 16:
+    pars = requests.get('https://olimp.miet.ru/ppo_it/api')
+    info = pars.json()
+    if info not in lt:
+        lt.append(info)
+    else:
+        t += 1
+
+
+for i in lt:
+    print(i)
+print(len(lt))
+print(t)
+a = json.dumps(lt)
+with open('data.jsonl', 'w') as f:
+    f.write(a)
